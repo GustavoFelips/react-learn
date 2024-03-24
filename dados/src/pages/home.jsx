f18fee65a6eaeacfd07d26ea2477346e3fc60e9c
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-03-24 18:22:05 +0000

    add placar

--- dados/src/pages/home.jsx
+++ dados/src/pages/home.jsx
@@ -2,19 +2,40 @@ import React, { useState } from "react";
 import { Dado } from '../components/dado';
 
 export default function Home() {
+
+  function Placar({ pontosJogador1, pontosJogador2 }) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <h3>Placar</h3>
+        <p>Jogador 1: {pontosJogador1}</p>
+        <p>Jogador 2: {pontosJogador2}</p>
+      </div>
+    );
+  }
+
+  const [vitoriaj1, setVitoriaj1] = useState(0)
+  const [vitoriaj2, setVitoriaj2] = useState(0)
+
   const [dado, setDado] = useState();
   const [dado2, setDado2] = useState();
   const [play, setPlay] = useState(true);
+
+  const ganhador = (valor) =>{
+    if(dado>valor){setVitoriaj1(vitoriaj1+1)}
+    else if(valor>dado){setVitoriaj2(vitoriaj2+1)}
+  }
   
   const handleClickJogar = () => {
     const valor = Math.floor(Math.random() * 6) + 1;
     setDado(valor);
     setPlay(!play);
   }
+
   const handleClickJogar2 = () => {
     const valor = Math.floor(Math.random() * 6) + 1;
     setDado2(valor);
     setPlay(!play);
+    ganhador(valor);
   }
   
   return (
@@ -31,8 +52,10 @@ export default function Home() {
           <Dado valor={dado2} />
           {!play ? <button onClick={handleClickJogar2}>Jogar Dado</button> : <p>Espere sua vez</p>}
         </div>
+        <Placar pontosJogador1={vitoriaj1} pontosJogador2={vitoriaj2}/>
       </div>
 
     </div>
   );
 }
+
